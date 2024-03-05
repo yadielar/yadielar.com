95b9a1a6fa5edd4883613380f0e6be201da28335
Yadiel Arroyo <yadielar@gmail.com>
2024-03-04 23:36:42 -0500

    improve styles for dark mode and print

---
+++ pages/_document.js
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html
+      lang="en"
+      className="text-gray-900 bg-white dark:bg-gray-900 dark:text-gray-100"
+    >
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -3,37 +3,37 @@ import Resume from '../components/resume.mdx';
 
 export default function Home() {
   return (
-    <div className="px-5">
+    <div className="px-5 print:px-0">
       <Head>
         <title>Yadiel Arroyo - Front-End Developer</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header className="flex flex-col items-center justify-center text-center mt-16">
-        <h1 className="text-4xl lg:text-6xl text-indigo-700 font-bold">
+      <header className="flex flex-col items-center justify-center text-center mt-16 print:mt-0">
+        <h1 className="text-4xl lg:text-6xl text-indigo-700 dark:text-indigo-300 font-bold">
           Yadiel Arroyo
         </h1>
         <p className="text-xl lg:text-3xl mt-4">Front-End Developer</p>
       </header>
 
       <main>
-        <article className="prose lg:prose-lg dark:prose-invert mx-auto mt-6 py-12">
+        <article className="prose prose-li:my-1 dark:prose-invert print:max-w-none mx-auto mt-6 py-12">
           <Resume />
         </article>
       </main>
 
-      <footer className="flex items-center justify-center border-t py-12">
+      <footer className="flex items-center justify-center border-t border-gray-200 dark:border-gray-800 py-12">
         <a
-          className="flex items-center justify-center text-gray-500 hover:text-blue-600"
+          className="flex items-center justify-center text-gray-500 hover:text-indigo-700 dark:hover:text-indigo-300"
           href="https://twitter.com/yadielar"
           target="_blank"
           rel="noopener noreferrer"
         >
           Twitter
         </a>
-        <span className="text-black/25 mx-4">|</span>
+        <span className="text-gray-300 dark:text-gray-700 mx-4">|</span>
         <a
-          className="flex items-center justify-center text-gray-500 hover:text-blue-600"
+          className="flex items-center justify-center text-gray-500 hover:text-indigo-700 dark:hover:text-indigo-300"
           href="https://github.com/yadielar"
           target="_blank"
           rel="noopener noreferrer"
